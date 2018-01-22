e9bc43337408ced469cebbf7070cec2e5a7f4b68
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2018-01-22 12:01:46 -0600

    added code formatting

--- controllers/home.js
+++ controllers/home.js
@@ -1,24 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+
+// ROUTES
+//home
 router.get('/', (req,res)=>{
   res.render('home.ejs');
 });
 
+//register
 router.get('/register', (req,res)=>{
   // res.render('register.ejs')
 });
 
 router.post('register', (req,res)=>{
 
-})
+});
 
-router.post('/', (req,res)=>{
+//login
+router.get('/login', (req,res)=>{
+  // res.render('login.ejs')
+});
+
+router.post('login', (req,res)=>{
 
 });
 
+//about
 router.get('/about', (req,res)=>{
-  // res.send('working')
   res.render('about.ejs')
 })
 
