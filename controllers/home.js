9cc8f46b8c53523bb6fae1ff84e722946bdc0e37
ethan375 <ethanebel1@gmail.com>
2018-01-22 14:12:02 -0600

    register post route now working

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const user = require('../models/user.js');
+const User = require('../models/user.js');
 const Post = require('../models/post.js');
 
 router.get('/', (req,res)=>{
@@ -13,11 +13,49 @@ router.get('/', (req,res)=>{
 });
 
 router.post('/', (req,res)=>{
-  //the first step is to find the user in the db 
-  user.findOne({username:req.body.username}, (err,user)=>{
-    if(user){
+  res.render('home.ejs');
+});
+
+router.get('/register', (req,res)=>{
+  console.log(req.session)
+  res.render('register.ejs')
+});
+
+router.post('/register', (req,res)=>{
+  //step 1 hash the password
+  //were setting the password the user entered to password variable
+  const password = req.body.password;
+  //now using bcrypt were encrypting the password
+  const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  //now creating an entry into the db with the password hash
+  const userDatabaseEntry = {
+    username: req.body.username,
+    password: passwordHash
+  }
+  console.log("\n\n\n\nHere is userDatabaseEntry:", userDatabaseEntry);
+  User.create(userDatabaseEntry, (err, userCreated)=>{
+    // console.log("this code is being run")
+    console.log(userCreated, "This is the userCreated");
+    //now that the userCreated has been created its time to set the session
+    console.log(req.session, "this is in our post route");
+    console.log(req.body.username);
+    req.session.logged = true;
+    console.log(req.session, "After we edited the properties")
+    res.redirect('/home');
+  });
+});
+
+
+router.get('/login', (req,res)=>{
+  res.render('login.ejs');
+});
+
+router.post('/login', (req,res)=>{
+    //the first step is to find the user in the db 
+  User.findOne({username:req.body.username}, (err,userFound)=>{
+    if(userFound){
       //we need to compare the password
-      if(bcrypt.compareSync(req.body.password, user.password)){
+      if(bcrypt.compareSync(req.body.password, userFound.password)){
         req.session.username = req.body.username;
         req.session.logged = true;
         req.session.message = '';
@@ -32,39 +70,6 @@ router.post('/', (req,res)=>{
       res.redirect('/home')//this is possibly going to be changed 
     }
   });
-
-  res.render('home.ejs');
-
-});
-
-router.get('/register', (req,res)=>{
-  console.log(req.session)
-  res.render('register.ejs')
-});
-
-router.post('register', (req,res)=>{
-  //step 1 hash the password
-  //were setting the password the user entered to password variable
-  const password = req.body.password;
-  //now using bcrypt were encrypting the password
-  const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  //now creating an entry into the db with the password hash
-  const userDatabaseEntry = {
-    username: req.body.username,
-    password: passwordHash
-  }
-
-  user.create(userDatabaseEntry, (err, user)=>{
-    console.log(user, "This is the user");
-    //now that the user has been created its time to set the session
-    console.log(req.session, "this is in our post route");
-    req.session.username = req.body.username;
-    req.session.logged = true;
-    console.log(req.session, "After we edited the properties")
-    res.redirect('/home');
-  });
-
-
 });
 
 
