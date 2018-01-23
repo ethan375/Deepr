b7b9b98435c3bd4a0483075c21e402effdd0a565
ethan375 <ethanebel1@gmail.com>
2018-01-23 11:28:29 -0600

    can multiple posts

--- models/comment.js
+++ models/comment.js
@@ -3,13 +3,12 @@ const mongoose = require('mongoose');
 const commentSchema = new mongoose.Schema({
 	username: {
 		type: String,
-		require: true,
-		unique: true
+		require: true
 	},
 	upvotes: Number,
 	body: String,
 })
 
-const Comment = mongoose.model('Comment', commentSchema);
 
-module.exports = Comment;
+
+module.exports = commentSchema;
