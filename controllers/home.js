7c17b0b2e6315980c474ecd9b5566b681d25acb1
ethan375 <ethanebel1@gmail.com>
2018-01-26 08:35:43 -0600

    up and down vote routes working

--- controllers/home.js
+++ controllers/home.js
@@ -214,17 +214,25 @@ router.get('/about', (req,res)=>{
 
 // upvoting
 router.post('/vote/:id', (req,res)=>{
-  console.log("this route being hit but not fully working")
+  // console.log("this route being hit")
   Post.findByIdAndUpdate(req.params.id, (err, foundPost)=>{
-    console.log(foundPost)
+    Post.votes += 1;
+    foundPost.save((err, savedPost)=>{
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(savedPost);
+      }
+    })
   })
 })
 
 //downvoting
 router.post('/downvote/:id', (req,res)=>{
-  console.log("this route being hit")
+  // console.log("this route being hit")
   Post.findByIdAndUpdate({id:req.params.id}, (err, foundPost)=>{
-    console.log(foundPost);
+    // console.log(foundPost);
+    Post.votes -=1
     foundPost.save((err, savedFoundPost)=>{
       if(err){
         console.log(err)
